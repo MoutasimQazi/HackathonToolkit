9a704cc2db0cd1ad1d533d3fd540021ff27d3952
Moutasim <115083371+MoutasimQazi@users.noreply.github.com>
2024-07-16 22:52:50 +0530

    commit

--- setup.py
+++ setup.py
@@ -20,11 +20,11 @@ with open('CHANGELOG.txt', 'r', encoding='utf-8') as f:
 
 setup(
   name='HackathonToolkit',
-  version='0.0.3',
+  version='0.0.4',
   description='The Hackathon Toolkit simplifies hackathon projects with utilities for setup, API interaction, data handling, ML, visualization, collaboration, and deployment, automating tasks for innovation focus.',
   long_description=long_description,
-  long_description_content_type='text/markdown',  # Adjust content type based on your actual format
-  url='',  # Update with your project's URL
+  long_description_content_type='text/markdown',  
+  url='', 
   author='Moutasim Qazi',
   author_email='moutasimqazi@gmail.com',
   license='MIT',
